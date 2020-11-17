2e1166b558185808e65122e559ada26328c8c1d2
Odinhengo <39441835+Odinhengo@users.noreply.github.com>
2020-11-17 15:05:17 -0300

    Update editor.py

--- src/editor.py
+++ src/editor.py
@@ -1,11 +1,23 @@
+from pygame importo midi
 
 class Editor:
     """
     Class that manage the editor
         for composing the musics
     """
-    def __init__(self, stream_of_char = '', cursor_pos=0, actual_char):
-        self.stream_of_char = stream
+    def __init__(self, pygame_editor, stream_char = '', cursor_pos=0, stream_selected):
+        self.stream_char = stream_char
         self.cursor_pos = cursor_pos
-        self.actual_char = actual_char
-
+        self.stream_selected = stream_selected
+        
+    def copy();
+        """ Copy the selected text and save it in the clipboard """
+        pass
+    
+    def cut():
+        """ Copy the selected text and delete it """
+        pass
+    
+    def paste():
+        """ Paste the text on the clipboard on the cursor position """
+        pass
